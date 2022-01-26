0de8d539f001669c2038b6c0d04c884500bfb229
Isaiah Poole <IPoole@thegbsgroup.us>
2022-01-26 10:05:29 -0500

    Fixed typo with import in Scene

--- Scene.py
+++ Scene.py
@@ -1,5 +1,5 @@
 from pygame import *
-from abc import ABC, abstract method
+from abc import ABC, abstractmethod
 
 # Represents a game scene
 # Scenes must inherit this class's functions in order to be used to create menus, graphics, logic, etc.
@@ -15,17 +15,17 @@ class Scene(ABC):
     # Called by Director and defined on the subclass
     @abstractmethod
     def on_update(self):
-        
-        raise NotImplementedError("on_update abstract method must be defined in subclass")
+        return 
+        # raise NotImplementedError("on_update abstract method must be defined in subclass")
 
     # Called to check for events (inputs)
     @abstractmethod
     def on_event(self, event):
-        
-        raise NotImplementedError("on_event abstract method must be defined in subclass")
+        return 
+        # raise NotImplementedError("on_event abstract method must be defined in subclass")
 
     # Is called when you want to draw on the screen
     @abstractmethod
     def on_draw(self, screen):
-        
-        raise NotImplementedError("on_Draw abstract method must be defined in subclass")
+        return 
+        # raise NotImplementedError("on_Draw abstract method must be defined in subclass")
